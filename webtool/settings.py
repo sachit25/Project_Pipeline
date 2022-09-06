849a781f61d06b363196b46af284166d96ca8277
unknown <Sachit.Gupta@skylinesemi.com>
2022-09-06 16:05:24 +0530

    settings file modified

--- webtool/settings.py
+++ webtool/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-jmps#*u$+*^pxl3t4_u#8&p0z$pbi11i$g)bjvwj_!-0&_=e^@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ec2-3-142-147-6.us-east-2.compute.amazonaws.com"]
+ALLOWED_HOSTS = ["ec2-3-142-147-6.us-east-2.compute.amazonaws.com","3.142.147.6:8000/"]
 
 
 # Application definition
